25b9b0a99450aa56ef107a21e7e383fb74c9d705
Muhammad Abuzar <syed.abuzar.badar@gmail.com>
2023-06-01 15:50:24 +0400

    added header component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,46 @@
 import React from 'react';
 
+import Image from '../assets/avatar.svg';
+
+import { FaGithub, FaYoutube, FaDribble } from 'react-icons/fa';
+import { TypeAnimation } from 'react-type-animation';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants';
+
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <section className='section' id='home'>
+      <div className='container mx-auto'>
+        <div>
+          <div>
+            <h1>
+              BEN <span>AIDEN</span>
+            </h1>
+            <div>
+              <span>I am a</span>
+              <TypeAnimation
+                sequence={[
+                  'Developer',
+                  2000,
+                  'Designer',
+                  2000,
+                  'Youtuber',
+                  2000,
+                ]}
+                speed={50}
+                className='text-accent'
+                wrapper='span'
+                repeat={Infinity}
+              />
+            </div>
+          </div>
+          <div>
+            <img src={Image} alt='' />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
