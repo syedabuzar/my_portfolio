4d1cb8d6936962b521c4e7c8332ec02476a0f4a6
Muhammad Abuzar <syed.abuzar.badar@gmail.com>
2023-06-26 04:51:28 +0400

    completed banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,22 +2,37 @@ import React from 'react';
 
 import Image from '../assets/avatar.svg';
 
-import { FaGithub, FaYoutube, FaDribble } from 'react-icons/fa';
+import { FaGithub, FaYoutube } from 'react-icons/fa';
 import { TypeAnimation } from 'react-type-animation';
 import { motion } from 'framer-motion';
 import { fadeIn } from '../variants';
 
 const Banner = () => {
   return (
-    <section className='section' id='home'>
+    <section
+      className='min-h-[85vh] lg:min-h-[78vh] flex items-center'
+      id='home'
+    >
       <div className='container mx-auto'>
-        <div>
-          <div>
-            <h1>
+        <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
+          <div className='flex-1 text-center font-secondary lg:text-left'>
+            <motion.h1
+              variants={fadeIn('up', 0.3)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'
+            >
               BEN <span>AIDEN</span>
-            </h1>
-            <div>
-              <span>I am a</span>
+            </motion.h1>
+            <motion.div
+              variants={fadeIn('up', 0.4)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1] '
+            >
+              <span className='text-white mr-4'>I am a </span>
               <TypeAnimation
                 sequence={[
                   'Developer',
@@ -32,11 +47,52 @@ const Banner = () => {
                 wrapper='span'
                 repeat={Infinity}
               />
-            </div>
+            </motion.div>
+            <motion.p
+              variants={fadeIn('up', 0.5)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='mb-8 max-w-lg mx-auto lg:mx-0'
+            >
+              kajshdk\ashjakshdakjhdkajhsajkdhkasjdhadkjashdkjashdkjashkdjahdkjahjk.
+              ashdkajshdkjahdkajhdkajhdkajdhaksjdhsajk. dasdasdjahahsd
+            </motion.p>
+            <motion.div
+              variants={fadeIn('up', 0.6)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'
+            >
+              <button className='btn btn-ig'>Contact Me</button>
+              <a href='#' className='text-gradient btn-link'>
+                My Portfolio
+              </a>
+            </motion.div>
+            <motion.div
+              variants={fadeIn('up', 0.7)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'
+            >
+              <a href='#'>
+                <FaYoutube />
+              </a>
+              <a href='#'>
+                <FaGithub />
+              </a>
+            </motion.div>
           </div>
-          <div>
+          <motion.div
+            variants={fadeIn('down', 0.5)}
+            initial='hidden'
+            whileInView={'show'}
+            className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]'
+          >
             <img src={Image} alt='' />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
